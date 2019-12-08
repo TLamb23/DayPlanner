6242e3aa3652ea78b0d9a690a7d9b0c7b9f366d0
Christopher Melby <melby.christopher@gmail.com>
2019-12-08 11:58:40 -0800

    append current date to header complete. Note: to always check order of scripts to save time in the future

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-//---define pointer variables----------//
 
-//---define any arrays/objs we need to create----------//
+
+// //---define pointer variables----------//
+
+
+
+//----define moment varibles used to show time----//
+var date = moment().format("MMM Do YY");
+moment().format('LT');
+
+
+// //-------append date----------//
+$("#currentDay").append(date);
+console.log(date);
+
+
+// //---define any arrays/objs we need to create----------//
+
+                                        
+
 
 //---funcitons----------//
 
