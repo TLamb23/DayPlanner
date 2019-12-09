3e85e60613fb54dbb9e318174595c291e0ccdb6c
Christopher Melby <melby.christopher@gmail.com>
2019-12-08 22:01:48 -0800

    finished styling css and created the onclick and storage/getitem to keep user input on the page.

--- script.js
+++ script.js
@@ -1,52 +1,168 @@
 
 
-// //---define pointer variables----------//
-var userInput = document.querySelector(".form-control");
-var saveButton = document.querySelector(".saveBtn");
 
 //----define moment varibles used to show time----//
 var date = moment().format("MMM Do YY");
 moment().format('LT');
 
 
-// //-------append date----------//
+//-------append date----------//
 $("#currentDay").append(date);
 console.log(date);
 
 
-// //---define any arrays/objs we need to create----------//
+//--onclick event to save user input to local storage---//
+$(".rowBtn").on("click", function() {
+    var hour = $(this)
+    .parent()
+    .attr("id")
 
-                                        
+    var textContent = $(this)
+    .siblings("input")
+    .val();
+localStorage.setItem(hour, textContent);
+console.log(hour, textContent);
+});
 
+//-------Getting individual childern and setting to parent-----//
+$("#9am")
+.children("input")
+  .val(localStorage.getItem("9am"));
 
-//---funcitons----------//
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+   
+  });
 
-function renderUserContent() {
-    var UserContent = localStorage.getItem("UserContent");
+  $("#10am")
+  .children("input")
+    .val(localStorage.getItem("10am"));
   
-    if (userContent === null) {
-      return;
-    }
-  
-    userInput.textContent = UserContent;
-  
-  }
-
-//---event listeners----------//
-
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-  
-    var userContent = document.querySelector(".form-control").value;
-      localStorage.setItem("userContent", userContent);
-  
+    $(".rowBtn").on("click", function() {
+      var hour = $(this)
+        .parent()
+        .attr("id");
+      var textContent = $(this)
+        .siblings("input")
+        .val();
+      localStorage.setItem(hour, textContent);
+      
     });
-  
+
+    $("#11am")
+.children("input")
+  .val(localStorage.getItem("11am"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+    
+  });
+
+  $("#12pm")
+.children("input")
+  .val(localStorage.getItem("12pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+    
+  });
+
+  $("#1pm")
+.children("input")
+  .val(localStorage.getItem("1pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+
+  });
+
+  $("#2pm")
+.children("input")
+  .val(localStorage.getItem("2pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+   
+  });
+
+  $("#3pm")
+.children("input")
+  .val(localStorage.getItem("3pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+    
+  });
+
+  $("#4pm")
+.children("input")
+  .val(localStorage.getItem("4pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+
+  });
+
+  $("#5pm")
+.children("input")
+  .val(localStorage.getItem("5pm"));
+
+  $(".rowBtn").on("click", function() {
+    var hour = $(this)
+      .parent()
+      .attr("id");
+    var textContent = $(this)
+      .siblings("input")
+      .val();
+    localStorage.setItem(hour, textContent);
+   
+  });
 
 
-// Next Steps :
+  // Next Steps :
 //
 // write a function to update the HTML input tag background depending on the time of day.
 //
 // store user input locally and be able to have that saved when the page is refreshed.
-
