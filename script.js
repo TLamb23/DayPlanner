2bbb6f15d5bb10c3d5dfc49d8dc74482f2131068
Christopher Melby <melby.christopher@gmail.com>
2019-12-08 16:25:41 -0800

    updating repository with recent changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 // //---define pointer variables----------//
-
-
+var userInput = document.querySelector(".form-control");
+var saveButton = document.querySelector(".saveBtn");
 
 //----define moment varibles used to show time----//
 var date = moment().format("MMM Do YY");
@@ -21,6 +21,32 @@ console.log(date);
 
 //---funcitons----------//
 
+function renderUserContent() {
+    var UserContent = localStorage.getItem("UserContent");
+  
+    if (userContent === null) {
+      return;
+    }
+  
+    userInput.textContent = UserContent;
+  
+  }
+
 //---event listeners----------//
 
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var userContent = document.querySelector(".form-control").value;
+      localStorage.setItem("userContent", userContent);
+  
+    });
+  
+
+
+// Next Steps :
+//
+// write a function to update the HTML input tag background depending on the time of day.
+//
+// store user input locally and be able to have that saved when the page is refreshed.
 
